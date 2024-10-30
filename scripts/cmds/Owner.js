96fb9bc1eb1eb987f109a0c91221dabf567b79b4
Ashikbot999 <sx74567@gmail.com>
2024-10-31 03:27:25 +0600

    Update Owner.js

--- scripts/cmds/Owner.js
+++ scripts/cmds/Owner.js
@@ -12,7 +12,7 @@ module.exports = {
 
  onStart: async function(){}, 
  onChat: async function({ event, message, getLang }) {
- if (event.body && event.body.toLowerCase() === "Oner") {
+ if (event.body && event.body.toLowerCase() === "Owner") {
  return message.reply({
  body: " 「𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥 𝗔𝗡𝗗 𝗔𝗗𝗠𝗜𝗡 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗜𝗢𝗡\n𝗡𝗔𝗠𝗘: BOSS~ASHIK 𝗪𝗛𝗔𝗧𝗦\n𝗔𝗣𝗣:+8801647711001\n𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/profile.php?id=100095089282395&mibextid=ZbWKwL\n𝗔𝗗𝗗𝗥𝗘𝗦𝗦: 𝗦𝗔𝗥𝗜𝗦𝗛𝗔 𝗕𝗔𝗥𝗜, 𝗝𝗔𝗠𝗔𝗟𝗣𝗨𝗥」",
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/JtUze7O.jpeg")
