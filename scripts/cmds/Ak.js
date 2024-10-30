c8b72db25ac3718951baa5607ab04b259a83157c
Ashikbot999 <sx74567@gmail.com>
2024-10-31 02:48:59 +0600

    Update Ak.js

--- scripts/cmds/Ak.js
+++ scripts/cmds/Ak.js
@@ -15,7 +15,7 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "ashikkoi") {
  return message.reply({
  body: " 「 「 🥰❥︎----ღ᭄_আশিক বস❞࿐.❤../n❥︎----ღ᭄_ ছেঁকা খাইছে❞࿐.🌴🤎🙂./n❥︎----ღ᭄_কান্না করতাছে❞࿐.🌹..../n❥︎---জা বলার.༎༅..🤷‍♂🍀./n❥︎----ღ᭄_আমারে বলতে পারেন❞࿐.🙂\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\nBOSS~ASHIK」",
- attachment: await global.utils.getStreamFromURL("https://video.xx.fbcdn.net/v/t42.3356-2/464971449_8538962602824864_8901947319593602145_n.mp4?_nc_cat=107&ccb=1-7&_nc_sid=4f86bc&_nc_eui2=AeFYRQ-WPVcBhgxNB1_iXEDxQ9QQNV_kqydD1BA1X-SrJ85R1EYvYrS5Md7NntFgEm5qepzQ6d6tZfPKak2c45YR&_nc_ohc=sv8uROlXFIIQ7kNvgFOvcwJ&_nc_zt=28&_nc_ht=video.xx&_nc_gid=AqpaZLGpH0z451C5WvdzmML&oh=03_Q7cD1QF5npZKoGoKujtXuT20e0AwaftHNCQ0ZyotGElo2E4UIQ&oe=6722A58E&dl=1")
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/9I65yEg.mp4")
  });
  }
  }
