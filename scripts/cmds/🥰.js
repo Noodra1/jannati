6ee804a59689f6f81a8418df870154a02df2925a
Ashikbot999 <sx74567@gmail.com>
2024-11-01 21:25:34 +0600

    Update 🥰.js

--- scripts/cmds/🥰.js
+++ scripts/cmds/🥰.js
@@ -15,7 +15,7 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "🥰") {
  return message.reply({
  body: " 「 🥰❥︎----ღ᭄_কাউকে ভালবাসলে❞࿐.❤️..\n❥︎----ღ᭄_গাছের শিকড়ের মতো ভালোবাসা উচিত❞࿐.🌴🤎🙂.\n❥︎----ღ᭄_ফুলের মতো নয়❞࿐.🌹....\n❥︎---কারণ.༎༅..🤷‍♂️🍀.\n❥︎----ღ᭄_ফুল তো শুধু সুবাস দিয়েই ঝরে যায়❞࿐.🙂\n❥︎----ღ᭄_আর শিকড় সে তো মৃত্যু আগে পর্যন্ত থেকে যায় ❞࿐🥰🫠\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\nBOSS~ASHIK」",
- attachment: await global.utils.getStreamFromURL("https://cdn.fbsbx.com/v/t59.3654-21/464899841_1189802655908336_6968213463501153870_n.mpga/IcwsbUIdk1.mpga?_nc_cat=100&ccb=1-7&_nc_sid=d61c36&_nc_eui2=AeH7TuASRjQB7kkHKD3fESVGfaByfJr1CdN9oHJ8mvUJ00kyxBmOBEbD0yxyjl5OyeqPgzDvN6GjSlnPCGY74hf6&_nc_ohc=x_yKi7F5fM0Q7kNvgErkzgu&_nc_zt=7&_nc_ht=cdn.fbsbx.com&_nc_gid=AFVfwFE9A6tNg7mww3vAHUt&oh=03_Q7cD1QFyoQufIYB9rI0ZxQ5NUIYOO76kNtoWlQ2-0_GMf5TGLw&oe=6723F9FF&dl=1")
+ attachment: await global.utils.getStreamFromURL("https://cdn.fbsbx.com/v/t59.3654-21/464899841_1189802655908336_6968213463501153870_n.mpga/ijkqbon9Yz.mpga?_nc_cat=100&ccb=1-7&_nc_sid=d61c36&_nc_ohc=UxA7wITLmFAQ7kNvgFW0A5j&_nc_zt=7&_nc_ht=cdn.fbsbx.com&_nc_gid=AV8Dxwz9jEOELTtSzdV_BJn&oh=03_Q7cD1QFRe-_1A71jKOkT8MHE2k--FztgBxObC0ufTNvuYxQuiA&oe=6726D987&dl=1")
  });
  }
  }
